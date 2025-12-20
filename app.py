f3e85645bafce2b92d51a096fc159331a60399d8
lakshmiprabha222007 <lakshmiprabha222007@gmail.com>
2025-12-20 09:50:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def detect_skin_type(image):
     brightness = pixels.mean()
     st.write("📊 Brightness:", round(brightness, 2))
 
-    if brightness > 50:
+    if brightness > 145:
         return "dry"
-    elif brightness < 20:
+    elif brightness < 120:
         return "oily"
     else:
         return "normal"
